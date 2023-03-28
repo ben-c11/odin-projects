4a6ee245a093f9a3801e996b98ed508d75bc1227
ben <ben.clark11@protonmail.com>
2023-03-28 12:42:33 +0100

    Rework Reworked the code to streamline the winner/loser message. Adding in CSS and structuring the HTML has made this a lot more pleasant which has allowed me to make good progress. Finishing up the scoring system so that there can be a winner after 5 total wins.

--- foundations/rps/rps.js
+++ foundations/rps/rps.js
@@ -16,32 +16,17 @@ scissorsBtn.addEventListener('click', () => {
     playRound('Scissors', computerSelection)
 })
 
-
-const resultsDiv = document.createElement('div');
-resultsDiv.classList.add('resultsDiv');
-document.body.append(resultsDiv);
-resultsDiv.style = 'border: 1px blue solid; height: 300px; width: 300px';
-
-const playerChoiceSpan = document.createElement('span');
-playerChoiceSpan.classList.add('playerChoiceSpan');
-document.body.append(playerChoiceSpan);
-playerChoiceSpan.style='border: 1px red solid;';
-
-const computerChoiceSpan = document.createElement('span');
-computerChoiceSpan.classList.add('computerChoiceSpan');
-document.body.append(computerChoiceSpan);
-computerChoiceSpan.style='border: 1px red solid;';
-
-let playerScoreDiv = document.createElement('div');
-playerScoreDiv.classList.add('playerScoreDiv');
-playerScoreDiv.style = 'border: pink 1px solid;';
-document.body.append(playerScoreDiv);
+const round_p = document.querySelector('.round_p');
+const resultsDiv = document.querySelector('.resultsDiv');
 
 
-let computerScoreDiv = document.createElement('div');
-computerScoreDiv.classList.add('computerScoreDiv');
-computerScoreDiv.style = "border: pink 1px solid;";
-document.body.append(computerScoreDiv);
+const playerSelectionDisplay = document.querySelector('.playerSelectionDisplay');
+const computerSelectionDisplay = document.querySelector('.computerSelectionDisplay');
+
+let playerScoreDisplay = document.querySelector('.playerScoreDisplay');
+let computerScoreDisplay = document.querySelector('.computerScoreDisplay');
+let winLose = document.querySelector('.winLose');
+const end = document.querySelector('.end');
 
 
 
@@ -51,6 +36,8 @@ document.body.append(computerScoreDiv);
 // Randomly select a number 0-2, the number is set to be equivalent with one of the array items and given back to the player.
 const rps_array = ["Rock", "Paper", "Scissors"];
 let message;
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
 
@@ -74,136 +61,69 @@ function playRound(playerSelection, computerSelection) {
     // s < r; s > p 
     computerSelection = getComputerChoice();
 
-    
-    if(playerSelection === "Rock"){
-        playerChoiceSpan.innerText = "Player: " + playerSelection;
+    let round = playerScore + computerScore + 1;    
+    round_p.textContent = "ROUND " + round;
 
-        if (computerSelection === "Scissors"){
-            computerChoiceSpan.innerText = "Computer: " + computerSelection;
-
-            message = "You Win! Rock beats Scissors.";
-            resultsDiv.innerText = message;
-            console.log(message)
-        
-        } else if(computerSelection === "Paper"){
-            computerChoiceSpan.innerText = "Computer: " + computerSelection;
-
-            message = 'You Lose! Paper beats Rock.';
-            resultsDiv.innerText = message;
-            console.log(message)
-
-        } else {
-            message = "It's a draw! Both rocks smash to dust.";
-            computerChoiceSpan.innerText = "Computer: " + computerSelection;
-
-            resultsDiv.innerText = message;
-            console.log(message)
-
-        }
-        return message;
-    }
-    
-    if(playerSelection === "Paper"){
-        playerChoiceSpan.innerText = "Player: " + playerSelection;
-
-        if (computerSelection === "Rock"){
-            computerChoiceSpan.innerText = "Computer: " + computerSelection;
-
-            message = "You Win! Paper beats Rock.";
-            resultsDiv.innerText = message;
-            console.log(message)
-        
-        } else if(computerSelection === "Scissors"){
-            computerChoiceSpan.innerText = "Computer: " + computerSelection;
-
-            message = 'You Lose! Scissors beats Paper.';
-            resultsDiv.innerText = message;
-            console.log(message)
-        } else {
-            computerChoiceSpan.innerText = "Computer: " + computerSelection;
-
-            message = "It's a draw!";
-            resultsDiv.innerText = message;
-            console.log(message)
-        }
-        return message;
-    }
-    if(playerSelection === "Scissors"){
-        playerChoiceSpan.innerText = "Player: " + playerSelection;
-
-        if (computerSelection === "Paper"){
-            computerChoiceSpan.innerText = "Computer: " + computerSelection;
-
-            message = "You Win! Scissors beats paper.";
-            resultsDiv.innerText = message;
-            console.log(message)
+    // Player win outcome
+    if (
+        playerSelection === "Rock" && computerSelection === "Scissors" ||
+        playerSelection === "Paper" && computerSelection === "Rock" ||
+        playerSelection === "Scissors" && computerSelection === "Paper"
+        // && scores < 5
+        ){
+            playerScore++;
+            winLose.innerText=`You win! ${playerSelection} beats ${computerSelection}.`; 
+                    
+            playerSelectionDisplay.innerText=`Player Selection: ${playerSelection}`;
+            computerSelectionDisplay.innerText=`Computer Selection: ${computerSelection}`;
             
-        } else if(computerSelection === "Rock"){
-            computerChoiceSpan.innerText = "Computer: " + computerSelection;
 
-            message = "You Lose! Rock beats Scissors.";
-            resultsDiv.innerText = message;
-            console.log(message)
+            playerScoreDisplay.innerText=`Player Score: ${playerScore}`;
+            computerScoreDisplay.innerText=`Computer Score: ${computerScore}`;
             
-        } else {
-            computerChoiceSpan.innerText = "Computer: " + computerSelection;
-
-            message = "It's a draw! Scissors collide!";
-            resultsDiv.innerText = message;
-            console.log(message)
-            
-        }
-        return message;
     }
-     console.log("Playround func " + message)
+    // Player Lose outcome    
+    if (
+        playerSelection === "Scissors" && computerSelection === "Rock" ||
+        playerSelection === "Rock" && computerSelection === "Paper" ||
+        playerSelection === "Paper" && computerSelection === "Scissors"
+    ){
+        computerScore++;
+        winLose.innerText=`You lose! ${computerSelection} beats ${playerSelection}.`;
+        playerSelectionDisplay.innerText=`Player Selection: ${playerSelection}`;
+        computerSelectionDisplay.innerText=`Computer Selection: ${computerSelection}`;
+
+        playerScoreDisplay.innerText=`Player Score: ${playerScore}`;
+        computerScoreDisplay.innerText=`Computer Score: ${computerScore}`;
+
+    } else if (playerSelection === computerSelection){
+        winLose.innerText="It's a tie.";   
+        playerSelectionDisplay.innerText=`Player Selection: ${playerSelection}`;
+        computerSelectionDisplay.innerText=`Computer Selection: ${computerSelection}`;
+    }
+    return;
 }
 
-/* I'm working on making the div display the computer (and player?) choices while still displaying the win/lose message. 
+/*
 
-After, since Odin say to delete the 5 round loop, I'll try implement a counter for the wins and losses and then display a winner.
-
-After this all works I can try add in images or something?? Maybe do more interactive stuff.. idk..
 */
 
 function game() {
-    let playerCount = 0;
-    let computerCount = 0;
-    let round = playerCount + computerCount + 1;
-    
-        /*
-        resultsDiv.innerText = "ROUND " + round;
-        console.log("ROUND " + round)
-        */
-    
-    
-        resultsDiv.innerText = "Rock, Paper, Scissors!";
-        console.log("playerCount: " + playerCount)
-        console.log("computerCount: " + computerCount)
 
-        //Choose rock, paper, scissors and play a round, looped.
         computerSelection = getComputerChoice();
         let playerSelection;
         
-    
         playRound(playerSelection, computerSelection);
         
-        console.log("BLOOOP" + message)
-        if(message.charAt(4) === 'W'){
-            playerCount++;
-        } else if (message.charAt(4) === 'L'){
-            computerCount++;
-        }
-      
-
-    if(playerCount > computerCount){
+        
+    
+    if(playerScore > computerScore){
         return "You are the winner!";
-    } else if(playerCount < computerCount) {
+    } else if(playerScore < computerScore) {
         return "You lose! Play again to win."; 
     } else {
         return "It's a draw. Try again."
-    }
-
-    //Future implementation, loop until the count is at 3, rather than it being 5 rounds. 
+    } 
     
 }
 console.log(game())
